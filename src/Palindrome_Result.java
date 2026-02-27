2e8acf143fcbbf26dc58dab721d3caee2d869ed2
tzar-beep <vaibhavkushwaha66@gmail.com>
2026-02-27 09:51:28 +0530

    Base application setup

---
+++ src/Palindrome_Result.java
@@ -0,0 +1,13 @@
+public class Palindrome_Result {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      PalindromeChecker App      ");
+        System.out.println("=================================");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Developed by: Stalin Gunasekaran");
+        System.out.println("=================================");
+
+    }
+}
