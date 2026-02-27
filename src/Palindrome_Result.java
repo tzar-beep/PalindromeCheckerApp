2a48a08b7ce3959ec81c21e5b591ac1cdf1550a2
tzar-beep <vaibhavkushwaha66@gmail.com>
2026-02-27 10:12:33 +0530

    UC2:Hardcoded palindrome

--- src/Palindrome_Result.java
+++ src/Palindrome_Result.java
@@ -2,12 +2,17 @@ public class Palindrome_Result {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("      PalindromeChecker App      ");
-        System.out.println("=================================");
-        System.out.println("Version: 1.0.0");
-        System.out.println("Developed by: Stalin Gunasekaran");
-        System.out.println("=================================");
+        String original = "madam";
+        String reversed = "";
 
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
